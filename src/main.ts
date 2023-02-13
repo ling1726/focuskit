e1d047a45e7c2ba9c953389121aed4eebadc1cec
Lingfan Gao <lingfangao@hotmail.com>
2023-02-13 15:57:35 +0100

    rename scenarios

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import './style.css'
 import './umd';
 
-import TrapScenario from "./scenarios/TrapAcenario";
-import DisabledScenario from './scenarios/DisabledScenario';
-import TrapGroupScenario from './scenarios/TrapGroupScenario';
-import NestedListTrapGroup from './scenarios/x';
-import ArrowZoneScenario from './scenarios/ArrowZoneScenario';
+import TrapScenario from "./scenarios/Trap";
+import DisabledScenario from './scenarios/Disabled';
+import TrapGroupScenario from './scenarios/TrapGroup';
+import ArrowZoneScenario from './scenarios/List';
 import ListGroup from './scenarios/ListGroup';
 import ListInTrapGroup from './scenarios/ListInTrapGroup';
 
