41142d25f6990ddc083e10c4ad57367e7df54471
Lingfan Gao <lingfangao@hotmail.com>
2023-02-18 11:13:10 +0100

    feat: useTrap should return imperative methods

--- packages/react/src/useTrap.ts
+++ packages/react/src/useTrap.ts
@@ -1,15 +1,34 @@
-import * as React from 'react';
-import { Trap } from '@focuskit/vanilla';
+import * as React from "react";
+import { Trap } from "@focuskit/vanilla";
 
-export function useTrap<TElement extends HTMLElement = HTMLElement>(id: string) {
+const defaultImperative = {
+  enable: () => null,
+  disable: () => null,
+};
+
+export function useTrap<TElement extends HTMLElement = HTMLElement>(
+  id: string
+) {
   const elementRef = React.useRef<TElement>();
+  const imperativeRef = React.useRef<{
+    enable: () => void;
+    disable: () => void;
+  }>(defaultImperative);
 
   React.useEffect(() => {
     if (elementRef.current) {
-      const list = new Trap(elementRef.current, { id });
-      return () => list.dispose();
+      const trap = new Trap(elementRef.current, { id });
+      imperativeRef.current = {
+        enable: () => trap.active = true,
+        disable: () => trap.active = false
+      }
+
+      return () => {
+        trap.dispose();
+        imperativeRef.current = defaultImperative;
+      }
     }
   }, [id]);
 
-  return elementRef;
+  return { elementRef, imperativeRef };
 }
